cbb88e1752995c23d61d416bb8bf0c29b78c3938
Amogh Warkhandkar <amoghw2025@gmail.com>
2025-01-03 22:15:45 +0100

    which-key Update

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -9,16 +9,24 @@ return {
     cond = not vim.g.vscode,
     config = function()
       local wk = require("which-key")
-      wk.register({
-        f = {
-          name = "File",
-          f = { "<cmd>Telescope find_files<cr>", "Find File" },
-          r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-        },
-        d = { name = "Delete/Close" },
-        q = { name = "Quit" },
-        u = { name = "UI" },
-      }, { prefix = "<leader>" })
+      -- wk.register({
+      --   f = {
+      --     name = "File",
+      --     f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      --     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      --   },
+      --   d = { name = "Delete/Close" },
+      --   q = { name = "Quit" },
+      --   u = { name = "UI" },
+      -- }, { prefix = "<leader>" })
+      wk.add({
+        { "<leader>d", group = "Delete/Close" },
+        { "<leader>f", group = "File" },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
+        { "<leader>q", group = "Quit" },
+        { "<leader>u", group = "UI" },
+      })
     end,
   }
 }
