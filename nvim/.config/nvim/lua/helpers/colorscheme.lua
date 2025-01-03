9d0f50e5f2f6824b710b535d0e5985b192b0894f
Amogh Warkhandkar <amoghw2025@gmail.com>
2025-01-03 15:15:17 +0100

    New Year 2025

--- nvim/.config/nvim/lua/helpers/colorscheme.lua
+++ nvim/.config/nvim/lua/helpers/colorscheme.lua
@@ -16,6 +16,6 @@ local function get_if_available(name, opts)
 end
 
 -- Uncomment the colorscheme to use
-local colorscheme = get_if_available("catppuccin-macchiato")
+local colorscheme = get_if_available("catppuccin-mocha")
 
 return colorscheme
